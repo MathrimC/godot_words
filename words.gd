189367dd9c03d0597a22f074dfd5fa7947a4c285
mathrim <mathrimcpoker@gmail.com>
2025-02-23 17:56:20 +0100

    keeping track of correct answers

--- words.gd
+++ words.gd
@@ -4,18 +4,24 @@ extends Node
 const words_path := "res://woordenlijst.txt"
 
 var words: Array[String]
+var letters: String
+var longest_answer: String
+var submitted_answers: Array[String]
 
 func _ready() -> void:
 	_load_words()
 
 ## Returns a new set of letters to use as an assignment for finding words
 func get_letters(amount_of_letters: int) -> String:
+	submitted_answers.clear()
 	var options: Array[String]
 	for word: String in words:
 		if word.length() == amount_of_letters:
 			options.append(word)
 	var word = options[randi_range(0, options.size())]
-	var letters: String = ""
+	longest_answer = word
+	word = word.to_lower()
+	letters = ""
 	while word.length() > 0:
 		var position := randi_range(0, word.length())
 		letters += word.substr(position,1)
@@ -23,16 +29,26 @@ func get_letters(amount_of_letters: int) -> String:
 	return letters
 
 ## Checks if all letters of the word appear in letters, and checks if the word exists in Dutch
-func check_word(word: String, letters: String) -> bool:
+func check_word(word: String) -> bool:
 	word = word.to_lower()
-	letters = letters.to_lower()
+	if submitted_answers.has(word):
+		return false
+	var letters_copy := letters
 	for letter in word:
-		var position := letters.find(letter)
+		var position := letters_copy.find(letter)
 		if position < 0:
 			return false
 		else:
-			letters = letters.erase(position,1)
-	return words.has(word)
+			letters_copy = letters_copy.erase(position,1)
+	if words.has(word):
+		submitted_answers.append(word)
+		return true
+	else:
+		return false
+
+## Returns the correct word using all the letters
+func get_longest_answer() -> String:
+	return longest_answer
 
 func _load_words() -> void:
 	var file := FileAccess.open(words_path, FileAccess.ModeFlags.READ)
--- words_test.gd
+++ words_test.gd
@@ -6,16 +6,15 @@ extends Control
 @export var response_label: Label
 @export var words: WordsNode
 
-var letters: String
-var answers: Array[String]
-
 func on_letters_button_pressed():
-	letters = words.get_letters(8)
-	letters_label.text = letters
+	letters_label.text = words.get_letters(8)
+	response_label.text = ""
 
 func on_input_submit(guess: String):
-	if !answers.has(guess) && words.check_word(guess, letters):
+	if words.check_word(guess):
 		response_label.text = "%s is correct!" % guess
-		answers.append(guess)
 	else:
 		response_label.text = "%s is wrong!" % guess
+
+func on_reveal_pressed():
+	response_label.text = "The full word was %s" % words.get_longest_answer()
